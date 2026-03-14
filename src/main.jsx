5188ac371eaba69237580ec3ef40d34d714ccc8f
alishagreenlaw-ops <alishagreenlaw@gmail.com>
2026-03-13 23:37:16 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CalmCanvas from './CalmCanvas'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CalmCanvas />
+  </React.StrictMode>
+)
